b5648db3851d6dd80bbfdfe772448f72cfb606ab
Cesim <cesimbayram.nl@gmail.com>
2026-04-15 23:33:54 +0200

    New animation met Js ,HTML,CSS
    New animation met Js ,HTML,CSS

--- scroll.js
+++ scroll.js
@@ -9,3 +9,29 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Navbar scroll efekti
+const topBar = document.querySelector(".top-bar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    topBar.classList.add("scrolled");
+  } else {
+    topBar.classList.remove("scrolled");
+  }
+});
+
+// Back to top
+const backToTopBtn = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add("visible");
+  } else {
+    backToTopBtn.classList.remove("visible");
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
